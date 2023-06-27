b3f59a43705045144b03712a5357365e7f04603c
Edwards Moses <edwardsmoses3@gmail.com>
2023-06-27 22:27:04 +0100

    feat: welcome world to nucleus

--- nucleus/ContentView.swift
+++ nucleus/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
                 Model3D(named: "Scene", bundle: realityKitContentBundle)
                     .padding(.bottom, 50)
 
-                Text("Hello, world!")
+                Text("Hello, world! Welcome to Nucleus")
 
                 Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
                     .toggleStyle(.button)
